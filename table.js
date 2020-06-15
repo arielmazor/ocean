620becbf82110a730b46fb9cc85dbfff64147ff9
ARIEL MAZOR <ariel.mazor.mail.com>
2020-06-15 20:08:28 +0300

    ...

---
+++ table.js
@@ -0,0 +1,16 @@
+function open(n) {
+debugger
+if ( n == 1){
+ 
+  window.open("file:///C:/Users/ariel/Documents/code/friendsNum.HTML");
+
+}
+if ( n == 2){
+  window.open("file:///C:/Users/ariel/Documents/code/galery.HTML");}
+if ( n == 3){
+  window.open("file:///C:/Users/ariel/Documents/code/id%20copy.HTML")
+}
+if ( n == 4){
+  window.open("file:///C:/Users/ariel/Documents/code/input.HTML")
+}
+}
